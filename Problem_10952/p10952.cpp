9a96b2311075e65c8901e37f2736fd605dd031b4
얌챠 <dbals9812@naver.com>
2020-09-27 13:05:03 +0900

    add solution of 10952

---
+++ Problem_10952/p10952.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+int main(void) {
+	int b, c;
+	while (true)
+	{
+		scanf("%d %d", &b, &c);
+		if (b == 0 && c == 0) {
+			break;
+		}
+		printf("%d\n", b + c);
+	}
+	return 0;
+}
