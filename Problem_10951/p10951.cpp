84658886d041ef040add63a7f87e2bc51d8718ec
얌챠 <dbals9812@naver.com>
2020-09-28 02:28:47 +0900

    edit solution of problem 10951

--- Problem_10951/p10951.cpp
+++ Problem_10951/p10951.cpp
@@ -4,8 +4,13 @@
 int main(void) {
 	int b, c;
 	while (true)
-	{
-		scanf("%d %d", &b, &c);
+	{	
+		// scanf returns the number of successfully received variables
+		int valid = scanf("%d %d", &b, &c);
+		// if "valid" are not 2, break and terminates while statement
+		if (valid!=2) {
+			break;
+		}
 		printf("%d\n", b + c);
 	}
 	return 0;
