fa066541e53251749a894f6f8c8a9fc31125f093
얌챠 <dbals9812@naver.com>
2020-12-24 12:19:33 +0900

    코드 템플릿

---
+++ 0. 템플릿/0.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(nullptr);
+	//cout.tie(nullptr);
+
+	cout << "qwer";
+	return 0;
+}
