43a3776bd93b4cd5c718fb3833125329fd18db0e
얌챠 <dbals9812@naver.com>
2020-12-24 12:32:17 +0900

    수정중

--- 2309. 일곱 난쟁이/2309_브루트포스.cpp
+++ 2309. 일곱 난쟁이/2309_브루트포스.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <string>
-#include <stack>
 using namespace std;
 
 int main(void) {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
+	int height[9];
+	int sum_of_height = 0;
+	for (int i = 0; i < 9; i++)
+	{	
+		//각각의 키를 받아서 입력하고, 합도 구함
+		cin >> height[i];
+		sum_of_height += height[i];
+	}
+
+	for()
+	int a;
+	scanf("%d", &a);
+	int b, c;
+	for (int i = 0; i < a; i++)
+	{
+		scanf("%d %d", &b, &c);
+		printf("%d\n", b + c);
+	}
 
 
 	return 0;
