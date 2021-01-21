84219815cab2e866b7a6a23f546ab3491096e569
얌챠 <dbals9812@naver.com>
2021-01-21 23:29:48 +0900

    (수정중)

---
+++ 2004. 조합 0의 개수/2004.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+	cout << "qwer";
+	return 0;
+}
