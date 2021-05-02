1561f3ec3c0ab4394f802f150075971ae27d629a
YAMCHA <dbals9812@naver.com>
2021-05-02 15:31:30 +0900

    add solution

--- 11653.cpp
+++ 11653.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 void Solution11653() {
@@ -6,10 +7,29 @@ void Solution11653() {
 
 	int input_number;	// 소인수분해할 수 (범위 1~10,000,000)
 	cin >> input_number;
+	int root_value = sqrt(input_number);
 
-	int a, b;
-	cin >> a >> b;
-	cout << "qwer";
+	bool breaked = false;
+	while (input_number > 1)
+	{
+		for (int i = 2; i <= root_value; i++)
+		{
+			if (input_number % i == 0) {
+				cout << i << "\n";
+				input_number = input_number / i;
+				breaked = true;
+				break;
+			}
+			else {
+				breaked = false;
+			}
+		}
+		if (!breaked) {
+			cout << input_number << "\n";
+			break;
+		}
+	}
+
 }
 
 int main(void) {
