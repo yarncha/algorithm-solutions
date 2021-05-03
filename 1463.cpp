4a1fcc9f367c64c5ffa9b0b5e5bdee0d9d55a99c
YAMCHA <dbals9812@naver.com>
2021-05-04 00:39:42 +0900

    add solution

--- 1463.cpp
+++ 1463.cpp
@@ -1,18 +1,50 @@
-//#include <iostream>
-//using namespace std;
-//
-//void solution() {
-//	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-//
-//	int input_number;
-//	cin >> input_number;
-//
-//		// 정수 x에 사용할 수 있는 연산이 3가지가 있음
-//	//이 연산들을 사용하여 어떤 정수 N을 1로 만들음.
-//	//	이때 연산을 사용하는 횟수의 최소값을 구하는 문제
-//	//top-down방식이랑 바텀엄방식 모두 해 보기
-//
-//	int a, b;
-//	cin >> a >> b;
-//	cout << "qwer";
-//}
+#include <iostream>
+using namespace std;
+
+// 구하려는 것 : input_number를 1로 만들기 위해 연산을 하는 횟수의 최솟값
+
+int RecursiveCalculator(int input_number, int* value_array) {
+	// 배열에 값이 있을 경우는 이를 바로 리턴해준다
+	if (input_number < 2) {
+		return 0;
+	}
+	else if (value_array[input_number] > 0) {
+		return value_array[input_number];
+	}
+
+	// 배열에 값이 없을 경우는 x/3, x/2 x-1중 가장 작은 것에 +1한 것을 배열에 넣고 리턴해준다
+	int smallest_value = RecursiveCalculator(input_number - 1, value_array);
+	int temp;
+	if (input_number % 3 == 0) {
+		temp = RecursiveCalculator(input_number / 3, value_array);
+		if (smallest_value > temp) {
+			smallest_value = temp;
+		}
+	}
+	if (input_number % 2 == 0) {
+		temp = RecursiveCalculator(input_number / 2, value_array);
+		if (smallest_value > temp) {
+			smallest_value = temp;
+		}
+	}
+
+	value_array[input_number] = smallest_value + 1;
+	return smallest_value + 1;
+}
+
+void Solution1463() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	int input_number;	// 범위 1~1000,000
+	cin >> input_number;
+
+	int min_number_of_calculation[1000001] = { 0, };
+	// min_number_of_calculation[x]는 x를 1로 만들기 위해 연산을 하는 횟수의 최솟값
+
+	cout << RecursiveCalculator(input_number, min_number_of_calculation);
+}
+
+int main(void) {
+	Solution1463();
+	return 0;
+}
