2fc8ae3693eb193432734f4d21adb11f19990f78
YAMCHA <dbals9812@naver.com>
2021-06-06 22:10:28 +0900

    edit template

--- 0.cpp
+++ 0.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 void Solution0() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	//ios_base::sync_with_stdio(false);
-	//cin.tie(nullptr);
-	//cout.tie(nullptr);
 
 	int a, b;
 	cin >> a >> b;
