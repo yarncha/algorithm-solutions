6b2560c5470b2c4dfb1923877e4a26f4734a0b12
YAMCHA <dbals9812@naver.com>
2021-06-15 01:51:52 +0900

    add problem

---
+++ 1107.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void Solution1107() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+}
+
+int main(void) {
+	Solution1107();
+	return 0;
+}
