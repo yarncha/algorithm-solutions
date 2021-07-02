ec801c1765bd2e6fc4733377e89affeda7ac20b6
YAMCHA <dbals9812@naver.com>
2021-07-03 03:15:32 +0900

    add problem

---
+++ 14226.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void Solution14226() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	
+}
+
+int main(void) {
+	Solution14226();
+	return 0;
+}
