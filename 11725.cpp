356e569bf1fa57de20651773f480727a26745aff
YAMCHA <dbals9812@naver.com>
2021-07-11 22:20:10 +0900

    add problem

---
+++ 11725.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+DFS BFS모두로 풀 수 있음
+*/
+
+void Solution11725() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+	cout << "qwer";
+}
+
+int main(void) {
+	Solution11725();
+	return 0;
+}
