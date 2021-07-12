7516086895868f6a85e5416bb4ab505d65460b15
YAMCHA <dbals9812@naver.com>
2021-07-12 23:49:27 +0900

    edit template

--- 0.cpp
+++ 0.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
+/*
+
+*/
+
 void Solution0() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	
 
-	int a, b;
-	cin >> a >> b;
-	cout << "qwer";
 }
 
 //int main(void) {
