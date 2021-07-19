e0c7493cce03fd76c47854845b8c8bbe0f34769a
YAMCHA <dbals9812@naver.com>
2021-07-19 23:37:07 +0900

    add problem

---
+++ 15649.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+중복 없이 M개를 구함, 순서와 관련된 문제임
+왜냐면 각각의 자리에 어떤 수가 들어가는 지 알아야 다음 수에 뭐가 들어갈지를 알 수 있기 때문
+*/
+
+void Solution15649() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+		
+}
+
+//int main(void) {
+//	Solution15649();
+//	return 0;
+//}
