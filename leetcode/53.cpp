2707c4327de8cbcce50a112ceebcb4715bca4548
YAMCHA <dbals9812@naver.com>
2021-10-29 16:58:06 +0900

    edit 53, constraints changed

--- leetcode/53.cpp
+++ leetcode/53.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 /*
 
-Runtime: 8 ms, faster than 46.46% of C++ online submissions for Maximum Subarray.
-Memory Usage: 13.1 MB, less than 88.59% of C++ online submissions for Maximum Subarray.
+Runtime: 104 ms
+Memory Usage: 68.2 MB
 
 */
 
 class Solution {
 public:
 	int maxSubArray(vector<int>& nums) {
-		int max_sum_of_subarray[30000];     // the max sum of subarray including last element i
+		int max_sum_of_subarray[100001];     // the max sum of subarray including last element i
 		int maximum = nums[0];
 		max_sum_of_subarray[0] = maximum;
 		for (int i = 1; i < nums.size(); i++)
@@ -24,7 +24,7 @@ public:
 				max_sum_of_subarray[i] = nums[i] + max_sum_of_subarray[i - 1];
 			}
 
-			if (maximum < max_sum_of_subarray[i]){
+			if (maximum < max_sum_of_subarray[i]) {
 				maximum = max_sum_of_subarray[i];
 			}
 		}
